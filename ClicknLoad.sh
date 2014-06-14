fbbe546d6441484642815f421a68306e2ac881ba
root <root@localhost.localdomain>
2014-06-14 11:45:24 +0200

    test

--- ClicknLoad.sh
+++ ClicknLoad.sh
@@ -2,7 +2,3 @@
 
 ssh -L 127.0.0.1:9666:127.0.0.1:9666 -N ip.zum.rasp.berry
 
-#SSH="ssh -v -N -f -L ${LOCALHOST}:${LOCALPORT}:${REMOTEHOST}:${REMOTEPO RT} ${REMOTELOGIN}"
-#SSHPID=`ps axww | grep "${SSH}" | grep -v 'grep'
-#kill ${SSHPID}
-
